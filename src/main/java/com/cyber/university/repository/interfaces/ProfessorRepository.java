85853c3e65641fc782008e574c6997def6b31d43
junyeoke <junyeoke@kakao.com>
2024-03-12 08:30:36 +0900

    merge 충돌 해결

--- src/main/java/com/cyber/university/repository/interfaces/ProfessorRepository.java
+++ src/main/java/com/cyber/university/repository/interfaces/ProfessorRepository.java
@@ -1,99 +1,83 @@
 package com.cyber.university.repository.interfaces;
 
-<<<<<<< HEAD
 import org.apache.ibatis.annotations.Mapper;
 
 import com.cyber.university.dto.professor.ProfessorInfoDto;
 import com.cyber.university.dto.professor.UpdateProfessorInfoDto;
-=======
 import java.util.List;
-
-import org.apache.ibatis.annotations.Mapper;
 
 import com.cyber.university.dto.ProfessorListForm;
 import com.cyber.university.repository.model.Professor;
 
->>>>>>> dev
-
 /**
-  * @FileName : ProfessorRepository.java
-  * @Project : CyberUniversity
-  * @Date : 2024. 3. 11. 
-<<<<<<< HEAD
-  * @작성자 : 장명근
-  * @변경이력 :
-  * @프로그램 설명 : 교수 Repository
-=======
-  * @작성자 : 이준혁
-  * @변경이력 :
-  * @프로그램 설명 : 교수 레파지토리
->>>>>>> dev
-  */
+ * @FileName : ProfessorRepository.java
+ * @Project : CyberUniversity
+ * @Date : 2024. 3. 11.
+ * @작성자 : 이준혁
+ * @변경이력 :
+ * @프로그램 설명 : 교수 레파지토리
+ */
 @Mapper
 public interface ProfessorRepository {
-	
-<<<<<<< HEAD
+
 	// 24.03.11 교수 내 정보 조회
 	public ProfessorInfoDto selectProfessorInfoWithCollegeAndDepartment(Integer id);
-	
+
 	// 24.03.11 교수 개인 정보 조회
 	public UpdateProfessorInfoDto selectProfessorInfo(Integer id);
-	
+
 	// 24.03.11 교수 정보 수정
 	public UpdateProfessorInfoDto updateProfessorInfo(UpdateProfessorInfoDto updateDto);
-	
+
 	// 24.03.11 비밀번호 찾기
 	public String selectPassword(Integer id);
-=======
+
 	/**
 	 * 
-	  * @Method Name : selectByProfessorId
-	  * @작성일 : 2024. 3. 11.
-	  * @작성자 : 이준혁
-	  * @변경이력 : 
-	  * @Method 설명 : id로 교수 조회
+	 * @Method Name : selectByProfessorId
+	 * @작성일 : 2024. 3. 11.
+	 * @작성자 : 이준혁
+	 * @변경이력 :
+	 * @Method 설명 : id로 교수 조회
 	 */
 	public List<Professor> selectByProfessorId(ProfessorListForm professorListForm);
-	
+
 	/**
 	 * 
-	  * @Method Name : selectByDepartmentId
-	  * @작성일 : 2024. 3. 11.
-	  * @작성자 : 이준혁
-	  * @변경이력 : 
-	  * @Method 설명 : 페이지별, 학과별 교수조회
+	 * @Method Name : selectByDepartmentId
+	 * @작성일 : 2024. 3. 11.
+	 * @작성자 : 이준혁
+	 * @변경이력 :
+	 * @Method 설명 : 페이지별, 학과별 교수조회
 	 */
 	public List<Professor> selectByDepartmentId(ProfessorListForm professorListForm);
-	
-	
+
 	/**
 	 * 
-	  * @Method Name : selectProfessorList
-	  * @작성일 : 2024. 3. 11.
-	  * @작성자 : 이준혁
-	  * @변경이력 : 
-	  * @Method 설명 : 페이지별 교수 조회
+	 * @Method Name : selectProfessorList
+	 * @작성일 : 2024. 3. 11.
+	 * @작성자 : 이준혁
+	 * @변경이력 :
+	 * @Method 설명 : 페이지별 교수 조회
 	 */
 	public List<Professor> selectProfessorList(ProfessorListForm professorListForm);
-	
+
 	/**
 	 * 
-	  * @Method Name : selectProfessorAmountByDeptId
-	  * @작성일 : 2024. 3. 11.
-	  * @작성자 : 이준혁
-	  * @변경이력 : 
-	  * @Method 설명 : 페이징 처리 학과별 교수 조회
+	 * @Method Name : selectProfessorAmountByDeptId
+	 * @작성일 : 2024. 3. 11.
+	 * @작성자 : 이준혁
+	 * @변경이력 :
+	 * @Method 설명 : 페이징 처리 학과별 교수 조회
 	 */
 	public Integer selectProfessorAmountByDeptId(Integer deptId);
-	
+
 	/**
-	  * @Method Name : selectProfessorAmount
-	  * @작성일 : 2024. 3. 11.
-	  * @작성자 : 이준혁
-	  * @변경이력 : 
-	  * @Method 설명 : 전체 교수 조회
+	 * @Method Name : selectProfessorAmount
+	 * @작성일 : 2024. 3. 11.
+	 * @작성자 : 이준혁
+	 * @변경이력 :
+	 * @Method 설명 : 전체 교수 조회
 	 */
 	public Integer selectProfessorAmount();
-
->>>>>>> dev
 }
--- src/main/java/com/cyber/university/service/ProfessorService.java
+++ src/main/java/com/cyber/university/service/ProfessorService.java
@@ -1,7 +1,7 @@
 package com.cyber.university.service;
 
-<<<<<<< HEAD
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -10,93 +10,82 @@ import com.cyber.university.dto.professor.UpdateProfessorInfoDto;
 import com.cyber.university.handler.exception.CustomRestfullException;
 import com.cyber.university.repository.interfaces.ProfessorRepository;
 import com.cyber.university.repository.model.User;
-=======
+
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cyber.university.dto.ProfessorListForm;
-import com.cyber.university.repository.interfaces.ProfessorRepository;
+
 import com.cyber.university.repository.model.Professor;
 
 /**
-  * @FileName : ProfessorService.java
-  * @Project : CyberUniversity
-  * @Date : 2024. 3. 11. 
-  * @작성자 : 이준혁
-  * @변경이력 :
-  * @프로그램 설명 : 교수 서비스
-  */
->>>>>>> dev
+ * @FileName : ProfessorService.java
+ * @Project : CyberUniversity
+ * @Date : 2024. 3. 11.
+ * @작성자 : 이준혁
+ * @변경이력 :
+ * @프로그램 설명 : 교수 서비스
+ */
 
 @Service
 public class ProfessorService {
-	
+
 	@Autowired
 	private ProfessorRepository professorRepository;
-	
-<<<<<<< HEAD
+
 	/**
-	  * @Method Name : selectProfessorInfoWithCollegeAndDepartment
-	  * @작성일 : 2024. 3. 11.
-	  * @작성자 : 장명근
-	  * @변경이력 : 
-	  * @Method 설명 : 교수 내 정보 조회 서비스
-	  */
+	 * @Method Name : selectProfessorInfoWithCollegeAndDepartment
+	 * @작성일 : 2024. 3. 11.
+	 * @작성자 : 장명근
+	 * @변경이력 :
+	 * @Method 설명 : 교수 내 정보 조회 서비스
+	 */
 	public ProfessorInfoDto selectProfessorInfoWithCollegeAndDepartment(Integer id) {
-		
-		return professorRepository.selectProfessorInfoWithCollegeAndDepartment(id); 
+
+		return professorRepository.selectProfessorInfoWithCollegeAndDepartment(id);
 	}
-	
-	
+
 	/**
-	  * @Method Name : selectProfessorInfo
-	  * @작성일 : 2024. 3. 11.
-	  * @작성자 : 장명근
-	  * @변경이력 : 
-	  * @Method 설명 : 교수 개인 정보 조회
-	  */
+	 * @Method Name : selectProfessorInfo
+	 * @작성일 : 2024. 3. 11.
+	 * @작성자 : 장명근
+	 * @변경이력 :
+	 * @Method 설명 : 교수 개인 정보 조회
+	 */
 	public UpdateProfessorInfoDto selectProfessorInfo(Integer id) {
-		
+
 		return professorRepository.selectProfessorInfo(id);
 	}
 
 	/**
-	  * @Method Name : updateProfessorInfo
-	  * @작성일 : 2024. 3. 11.
-	  * @작성자 : 장명근
-	  * @변경이력 : 
-	  * @Method 설명 : 교수 정보 수정 서비스
-	  */
+	 * @Method Name : updateProfessorInfo
+	 * @작성일 : 2024. 3. 11.
+	 * @작성자 : 장명근
+	 * @변경이력 :
+	 * @Method 설명 : 교수 정보 수정 서비스
+	 */
 	public UpdateProfessorInfoDto updateProfessorInfo(Integer id, User user) {
-		
+
 		String enteredPassword = user.getPassword();
-		
+
 		String savedPassword = professorRepository.selectPassword(id);
-		
+
 		if (savedPassword == null || !savedPassword.equals(enteredPassword)) {
 			throw new CustomRestfullException("비밀번호를 확인해주세요.", HttpStatus.BAD_REQUEST);
 		}
-		
+
 		UpdateProfessorInfoDto updateDto = new UpdateProfessorInfoDto();
-	    updateDto.setAddress(updateDto.getAddress());
-	    updateDto.setTel(updateDto.getTel());
-	    updateDto.setEmail(updateDto.getEmail());
-	    updateDto.setId(updateDto.getId());
-	    
-	    professorRepository.updateProfessorInfo(updateDto);
-	    
-	    return updateDto;
-	
+		updateDto.setAddress(updateDto.getAddress());
+		updateDto.setTel(updateDto.getTel());
+		updateDto.setEmail(updateDto.getEmail());
+		updateDto.setId(updateDto.getId());
+
+		professorRepository.updateProfessorInfo(updateDto);
+
+		return updateDto;
+
 	}
-	
-	
-	
-	
-	
-=======
 
 	/**
 	 * @param professorListForm
@@ -134,5 +123,4 @@ public class ProfessorService {
 		return amount;
 	}
 
->>>>>>> dev
 }
